f32f653d982d79f8857119f17433747c862147d8
Anish George <anishgeo@thoughtworks.com>
2018-07-31 23:24:06 +0530

    Add Cors and modify run scripts to use express always

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const jsonServer        = require('json-server')
-const server            = jsonServer.create()
+const jsonServer = require('json-server')
+const cors       = require('cors')
+const server     = jsonServer.create()
 
-const db = require('./db.json')
-const router = jsonServer.router(db);
+const isPersistent = process.argv[2] === 'persistent',
+      fileName = './db.json',
+			db     = isPersistent ? fileName : require(fileName),
+			router = jsonServer.router(db);
 
 const defaultMiddleware = jsonServer.defaults()
 
+server.use(defaultMiddleware)
 server.use(router)
+server.use(cors())
 
 server.listen(3000, () => {
 	console.log('JSON Server is running at http://localhost:3000/')
+	console.log("Mode: ", isPersistent ? 'Persistent' : 'In memory');
 })
